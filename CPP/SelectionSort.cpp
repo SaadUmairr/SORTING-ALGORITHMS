17cc7914925d32f3f08d680ad30c91fb723573fa
Saad Umair <saad.ansari0106@gmail.com>
2023-08-24 22:58:28 +0530

    Selection Sort

---
+++ CPP/SelectionSort.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <algorithm>
+#define vec vector<ll>
+#define ll long long int
+using namespace std;
+void Swap(ll &a, ll &b)
+{
+    a = a + b;
+    b = a - b;
+    a = a - b;
+}
+void PrintVec(vec arr)
+{
+    for (int i = 0; i < arr.size(); i++)
+        cout << arr[i] << '\t';
+    cout << endl;
+}
+void SelectionSort(vec &arr)
+{
+    int size = arr.size(), i, j, min;
+
+    for (i = 0; i < size - 1; i++)
+    {
+        min = i;
+        for (j = i + 1; j < size; j++)
+            if (arr[j] < arr[min])
+                min = j;
+        if (min != i)
+            Swap(arr[i], arr[min]);
+    }
+}
+
+int main()
+{
+    system("cls");
+    // ios_base::sync_with_stdio(false);
+    // cin.tie(NULL);
+    // #ifndef ONLINE_JUDGE
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    // #endif
+    int size, el;
+    cout << "Enter size of the array: ";
+    cin >> size;
+    vec arr;
+    for (int i = 0; i < size; i++)
+        cin >> el, arr.push_back(el);
+    cout << "ARRAY BEFORE SORTING:" << endl;
+    PrintVec(arr);
+    SelectionSort(arr);
+    cout << "ARRAY AFTER SORTING:" << endl;
+    PrintVec(arr);
+
+    return 0;
+}
