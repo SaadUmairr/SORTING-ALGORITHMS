bcfffa461a83128e57863961014251be56ee5622
Saad Umair <saad.ansari0106@gmail.com>
2023-08-24 11:05:56 +0530

    Optimised Bubble Sort

--- CPP/BubbleSort.cpp
+++ CPP/BubbleSort.cpp
@@ -14,11 +14,22 @@ void Swap(ll &a, ll &b)
 }
 void BubbleSort(vec &arr)
 {
+    bool change;
     int size = arr.size();
     for(int i=0;i<size;++i)
+    {
+        change = false;
         for(int j=0;j<size-i-1;++j)
+        {
             if(arr[j]>arr[j+1])
+            {
                 Swap(arr[j], arr[j+1]);
+                change = true;
+            }
+        }
+        if(!change)
+            break;
+    }
 }
 void PrintVec(vec arr)
 {
