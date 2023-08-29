7304ec068424e3ba465068f2eed523bbab9856c0
Saad Umair <saad.ansari0106@gmail.com>
2023-08-30 02:52:08 +0530

    COUNT SORT IN C++ Counting Sort is a linear time sorting algorithm that works well for integers within a known range. It counts the frequency of each element and uses this information to place them in sorted order.

---
+++ CPP/6-CountSort.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <algorithm>
+#define vec vector<ll>
+#define ll long long int
+using namespace std;
+
+void PrintVec(vec arr)
+{
+    for (int i = 0; i < arr.size(); i++)
+        cout << arr[i] << '\t';
+    cout << endl;
+}
+
+vec CountSort(vec &arr)
+{
+    ll high = arr[0], i;
+    for (i = 0; i < arr.size(); i++)
+        high = max(high, arr[i]);
+    vec freq(high + 1, 0);
+    for (i = 0; i < arr.size(); i++)
+        freq[arr[i]]++;
+    for (i = 1; i <= high; i++)
+        freq[i] += freq[i - 1];
+    vec sorted(arr.size());
+    for (i = high - 1; i >= 0; i--)
+        sorted[--freq[arr[i]]] = arr[i];
+    return sorted;
+}
+
+int main()
+{
+    system("cls");
+    // ios_base::sync_with_stdio(false);
+    // cin.tie(NULL);
+    // #ifndef ONLINE_JUDGE
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    // #endif
+    int size, el;
+    cout << "Enter size of the array: ";
+    cin >> size;
+    vec arr;
+    for (int i = 0; i < size; i++)
+        cin >> el, arr.push_back(el);
+    cout << "ARRAY BEFORE SORTING:" << endl;
+    PrintVec(arr);
+    arr = CountSort(arr);
+    cout << "ARRAY AFTER SORTING:" << endl;
+    PrintVec(arr);
+    return 0;
+}
