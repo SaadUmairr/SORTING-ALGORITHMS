296248e89f9f73fba25b6b668c1106129a18311b
Saad Umair <saad.ansari0106@gmail.com>
2023-09-25 15:24:43 +0530

    Implement Shell Sort in Python Shell sort is a highly efficient sorting algorithm and is based on insertion sort algorithm. This algorithm avoids large shifts as in case of insertion sort, if the smaller value is to the far right and has to be moved to the far left.

---
+++ Python/ShellSort.py
@@ -0,0 +1,34 @@
+def ShellSort(arr):
+    size = len(arr)
+    gap = size // 2
+    while gap > 0:
+        for j in range(gap, size):
+            i = j - gap
+            while i >= 0:
+                if arr[i + gap] > arr[i]:
+                    break
+                else:
+                    arr[i + gap], arr[i] = arr[i], arr[i + gap]
+                i -= gap
+        gap //= 2
+
+
+try:
+    arr = []
+    size = int(input("Enter the size of the list: "))
+    for _ in range(size):
+        while True:
+            try:
+                el = int(input("Enter a number: "))
+                arr.append(el)
+                break 
+            except ValueError:
+                print("Invalid input. Please enter a valid number.")
+
+    print("INPUT LIST:", arr)
+    ShellSort(arr)
+    print("SORTED LIST:", arr)
+except KeyboardInterrupt:
+    print("Program interrupted by the user.")
+except Exception as e:
+    print(f"INPUT MUST BE IN NUMBER")
