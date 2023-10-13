f1db01bbf129a9104938cfc15feff73eecf3e30f
Saad Umair <saad.ansari0106@gmail.com>
2023-10-14 01:25:30 +0530

    Add Tree Sort in Java Tree sort is a sorting algorithm that is based on Binary Search Tree data structure. It first creates a binary search tree from the elements of the input list or array and then performs an in-order traversal on the created binary search tree to get the elements in sorted order.

---
+++ Java/TreeSort.java
@@ -0,0 +1,80 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+public class TreeSort {
+    public static class TreeNode {
+        int data;
+        TreeNode left;
+        TreeNode right;
+
+        public TreeNode(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter size of the list: ");
+        try {
+            int size = scanner.nextInt();
+
+            if (size <= 0) {
+                System.out.println("Size of list can not be negative or zero");
+                scanner.close();
+                return;
+            }
+            System.out.println("Enter the elements: ");
+            for (int i = 0; i < size; i++) {
+                int el = scanner.nextInt();
+                arr.add(el);
+            }
+            System.out.println("INPUT LIST: ");
+            for (int num : arr)
+                System.out.print(num + "\t");
+            treeSort(arr);
+            System.out.println("\nSORTED LIST: ");
+            for (int num : arr)
+                System.out.print(num + "\t");
+        } catch (InputMismatchException e) {
+            System.out.println("INPUT MUST BE IN NUMBERS");
+        }
+        scanner.close();
+    }
+
+    public static void treeSort(ArrayList<Integer> arr) {
+        TreeNode root = null;
+
+        for (int value : arr) {
+            root = insert(root, value);
+        }
+
+        arr.clear();
+        inOrderTraversal(root, arr);
+    }
+
+    public static TreeNode insert(TreeNode root, int data) {
+        if (root == null) {
+            return new TreeNode(data);
+        }
+
+        if (data < root.data) {
+            root.left = insert(root.left, data);
+        } else if (data > root.data) {
+            root.right = insert(root.right, data);
+        }
+
+        return root;
+    }
+
+    public static void inOrderTraversal(TreeNode root, ArrayList<Integer> result) {
+        if (root != null) {
+            inOrderTraversal(root.left, result);
+            result.add(root.data);
+            inOrderTraversal(root.right, result);
+        }
+    }
+}
