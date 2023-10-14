32496cd68abc2dad6e48746648d4ceaebf5c7883
Saad Umair <saad.ansari0106@gmail.com>
2023-10-14 19:18:16 +0530

    Add Merge and Quick sort in JS complete implentation of these algo, previously it was only snippet

--- Javascript/MergeSort.js
+++ Javascript/MergeSort.js
@@ -1,9 +1,44 @@
-function MergeSort(arr){
-    
+function MergeSort(arr, left, right) {
+    if (left >= right) return;
+    let middle = left + Math.floor((right - left) / 2);
+    MergeSort(arr, left, middle);
+    MergeSort(arr, middle + 1, right);
+    ActualWorker(arr, left, middle, right);
 }
+
+function ActualWorker(arr, left, middle, right) {
+    let leftSide = middle - left + 1,
+        rightSide = right - middle;
+    let leftHalf = [],
+        rightHalf = [];
+    for (let i = 0; i < leftSide; i++) leftHalf[i] = arr[left + i];
+    for (let i = 0; i < rightSide; i++) rightHalf[i] = arr[middle + i + 1];
+    let i = 0,
+        j = 0,
+        k = left;
+    while (i < leftSide && j < rightSide) {
+        if (leftHalf[i] <= rightHalf[j]) {
+            arr[k] = leftHalf[i];
+            i++;
+        } else {
+            arr[k] = rightHalf[j];
+            j++;
+        }
+        k++;
+    }
+    while (i < leftSide) {
+        arr[k] = leftHalf[i];
+        i++, k++;
+    }
+    while (j < rightSide) {
+        arr[k] = rightHalf[j];
+        j++, k++;
+    }
+}
+
 let arr = [5, 4, 3, 2, 1];
 console.log("ARRAY BEFORE SORTING:");
 console.log(arr);
-MergeSort(arr);
+MergeSort(arr, 0, 4);
 console.log("ARRAY AFTER SORTING:");
 console.log(arr);
--- Javascript/QuickSort.js
+++ Javascript/QuickSort.js
@@ -1,9 +1,39 @@
-function QuickSort(arr){
-    
+function QuickSort(arr, low, high) {
+    if (low < high) {
+        let j = partition(arr, low, high);
+        QuickSort(arr, low, j - 1);
+        QuickSort(arr, j + 1, high);
+    }
 }
+
+function partition(arr, low, high) {
+    let pivot = arr[low];
+    let i = low + 1,
+        j = high;
+
+    while (true) {
+        while (i <= j && arr[i] <= pivot) {
+            i++;
+        }
+
+        while (i <= j && arr[j] > pivot) {
+            j--;
+        }
+
+        if (i <= j) {
+            [arr[i], arr[j]] = [arr[j], arr[i]];
+        } else {
+            break;
+        }
+    }
+
+    [arr[low], arr[j]] = [arr[j], arr[low]];
+    return j;
+}
+
 let arr = [5, 4, 3, 2, 1];
 console.log("ARRAY BEFORE SORTING:");
 console.log(arr);
-QuickSort(arr);
+QuickSort(arr, 0, arr.length - 1);
 console.log("ARRAY AFTER SORTING:");
 console.log(arr);
